0f09bdb108e2af3163b6beb12668df9253f3b785
Timothy Jay <tvsayson@student.apc.edu.ph>
2024-01-13 13:41:20 +0800

    Javascript Lesson 8 Dates

--- week5/script.js
+++ week5/script.js
@@ -10,3 +10,7 @@ document.getElementById("age").innerHTML = "<strong> Age: </strong>" + aged;
 /* Lesson 7 Arrays */
 const games = ["Valorant","League of Legends","Overwatch 2","Lethal Company"]
 document.getElementById("game").innerHTML = "These are the games I always play but you can still invite me on other games " + games
+
+/* Lesson 8 Dates */
+const dates = new Date();
+document.getElementById("date").innerHTML = dates;
