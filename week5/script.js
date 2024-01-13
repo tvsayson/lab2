2fff0b6310524d7ff73a03e362c5b7e9d2fc1457
Timothy Jay <tvsayson@student.apc.edu.ph>
2024-01-13 13:25:47 +0800

    Javascript Lesson 7 Arrays

--- week5/script.js
+++ week5/script.js
@@ -6,3 +6,7 @@ const age1 = 10;
 const age2 = 10;
 let aged = age1 + age2;
 document.getElementById("age").innerHTML = "<strong> Age: </strong>" + aged;
+
+/* Lesson 7 Arrays */
+const games = ["Valorant","League of Legends","Overwatch 2","Lethal Company"]
+document.getElementById("game").innerHTML = "These are the games I always play but you can still invite me on other games " + games
