c6829339750f69d4d121c775d007c867f61abb63
Timothy Jay <tvsayson@student.apc.edu.ph>
2024-01-13 13:05:26 +0800

    Javascript Lesson 2 Variables, Let, Const & Data Types

--- week5/script.js
+++ week5/script.js
@@ -1,1 +1,8 @@
+/*Lesson 1 */
 alert('Hi, this website is still under development! but hey, welcome to my site! 😁');
+
+/*Lesson 2 */
+const age1 = 10;
+const age2 = 10;
+let aged = age1 + age2;
+document.getElementById("age").innerHTML = "<strong> Age: </strong>" + aged;
