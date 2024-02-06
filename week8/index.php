d28d4a898301cf55613c3fa3f976f0b7de1b2627
Timothy Jay Sayson <71298147+tvsayson@users.noreply.github.com>
2024-02-06 12:18:46 +0800

    Update index.php

--- week8/index.php
+++ week8/index.php
@@ -217,10 +217,17 @@ echo $gender;
 ?>
 
 <?php
+// For Xampp Localhost
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "myDB";
+
+//For socit cloud
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDB";
+$username = "webprogmi221";
+$password = "g_6bCitLu.ljMK*m";
+$dbname = "webprogmi221";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
