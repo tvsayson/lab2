e47c6ddab7f66c54c26c70fbc45e898fa45c4ea1
Timothy Jay <tvsayson@student.apc.edu.ph>
2024-02-06 13:12:23 +0800

    Added a new code again

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender FROM MyGuests";
+$sql = "SELECT id, name, email, website, comment, gender FROM tvsayson_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
