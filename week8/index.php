8d9284df8033d9b75a0a9f1fa658a12ba6cf4a03
Timothy Jay Sayson <71298147+tvsayson@users.noreply.github.com>
2024-02-06 12:22:23 +0800

    Update index.php

--- week8/index.php
+++ week8/index.php
@@ -236,7 +236,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO MyGuests (name, email, website, comment, gender)
+$sql = "INSERT INTO tvsayson_myguests (name, email, website, comment, gender)
 VALUES ('$name', '$email', '$website', '$comment', '$gender')";
 
 if ($conn->query($sql) === TRUE) {
